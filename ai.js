ac07bebf8f198555e6417524f7164952d21fbef5
Zarifur <79872370+Zarifur@users.noreply.github.com>
2024-05-03 12:01:51 +0600

    up

--- ai.js
+++ ai.js
@@ -1,24 +1,58 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
+let weights = [0.0, 0.0, 0.0, 0.0];
+document.querySelectorAll(".weights input").forEach((el, idx) => {
+  el.addEventListener("input", (e) => {
+    weights[idx] = e.target.value;
+  });
+});
+
+document.querySelector(".vis").addEventListener("click", () => {
+  visual();
+});
+
+const visual = () => {
+  for (let i = 0; i <= canvas.width; i++) {
+    for (let j = 0; j <= canvas.height; j++) {
+      visualize(i, j);
+    }
+  }
+};
+const classify = (input_1, input_2) => {
+  let output_1 = input_1 * weights[0] + input_1 * weights[1];
+  let output_2 = input_2 * weights[2] + input_2 * weights[3];
+  return output_1 > output_2 ? 1 : 0;
+};
+
+const visualize = (x, y) => {
+  console.log(x);
+  console.log(y);
+  const cla = classify(x, y);
+  if (cla == 1) {
+    ctx.fillStyle = "#92383e";
+    ctx.fillRect(x, y, 10, 10);
+  } else {
+    ctx.fillStyle = "#1e3246";
+    ctx.fillRect(x, y, 10, 10);
+  }
+};
+
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 Papa.parse(data, {
   complete: function (results) {
     console.log("Finished:", results.data);
-    // da = data;
+
     let max_dia = 0;
     let max_stem = 0;
     for (let i = 1; i < results.data.length; i++) {
-      // console.log("Diameter :" + results.data[i][0]);
       if (max_dia < results.data[i][0] * 1) {
         max_dia = results.data[i][0] * 1;
       }
       if (max_stem < results.data[i][5] * 1) {
         max_stem = results.data[i][5] * 1;
       }
-      // console.log("stem-width :" + results.data[i][5]);
 
-      // console.log("Poisonous :" + results.data[i][8]);
       if (results.data[i][8] * 1 == 1) {
         ctx.fillStyle = "red";
       } else {
@@ -31,8 +65,8 @@ Papa.parse(data, {
         10
       );
     }
-
-    console.log(max_dia);
-    console.log(max_stem);
   },
 });
+class Node {}
+
+class layers {}
